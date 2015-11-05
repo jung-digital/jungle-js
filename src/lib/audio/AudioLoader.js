751c81bc36dbc700baa5c608faf53145777ff940
Ben Bryan <ben@irq5.net>
2015-11-04 23:28:56 -0600

    Add initial pass at AudioLoader class.

--- src/lib/audio/AudioLoader.js
+++ src/lib/audio/AudioLoader.js
@@ -10,27 +10,47 @@
  * Class
 \*============================================*/
 /**
- * Audio Loader class 
+ * Audio Loader class
  */
 class AudioLoader  {
   //---------------------------------------------
   // Constructor
   //---------------------------------------------
   /**
-   * 
+   *
    */
-  constructor() {
+  constructor(url, context) {
+    var AudioContext = window.AudioContext || window.webkitAudioContext;
+    this._url = url;
+    this._audioContext = context || new AudioContext();
+  }
 
+  //---------------------------------------------
+  // Methods
+  //---------------------------------------------
+  loadAudioFile() {
+    this._xhr = new XMLHttpRequest();
+    this._xhr.responseType = "arraybuffer";
+    this._xhr.onload = this._onAudioLoad();
   }
 
   //---------------------------------------------
   // Properties
   //---------------------------------------------
-  
+  var _url = "";
+  var _audioContext;
+  var _xhr;
+  var _loadedBuffer;
+
   //---------------------------------------------
   // Event Handlers
   //---------------------------------------------
-  
+  _onAudioLoad() {
+    this._audioContext.decodeAudioData(this._xhr.response, function(buffer) {
+      this._loadedBuffer = buffer;
+    });
+  }
+
 }
 
 export default AudioLoader;
