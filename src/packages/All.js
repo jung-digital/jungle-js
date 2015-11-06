2f7d172df662406de1e0c694cc1931c78539e9c7
joshjung <joshua.p.jung@gmail.com>
2015-11-05 19:06:50 -0600

    Fix Starfiled capitalization reference

--- src/packages/All.js
+++ src/packages/All.js
@@ -1,5 +1,5 @@
 import _Embers from '../components/Embers';
-import _Starfield from '../components/Starfield';
+import _Starfield from '../components/StarField';
 import _Fireworks from '../components/Fireworks';
 import Lib from '../lib/Jungle';
 
