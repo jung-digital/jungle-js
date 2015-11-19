3efaf0143cc34f8eb754c56c99db354c97d24d25
Joseph Jung <jungyue@gmail.com>
2015-11-18 21:25:53 -0600

    Intercom - Refactor a bit of the menu to accept JSON and be rendered as underscore template.

--- demos/menu/main.js
+++ demos/menu/main.js
@@ -1,37 +1,4 @@
 var menuContainer = document.querySelector(".menu-component");
+var configFilePath = "/menu-config.json";
 
-var jungleMenu = new Menu(menuContainer, $);
-
-// var jungleMenu = new Menu(menuContainer, {
-// 	items: [
-// 		{
-// 			name: "Home",
-// 			href: "/welcome.html"
-// 		},
-// 		{
-// 			name: "Login",
-// 			doesOpenSubmenu: true
-// 		},
-// 		{
-// 			name: "Contact Us",
-// 			href: "/contact.html"
-// 		},
-// 		{
-// 			name: "Container Tracing",
-// 			herf: "/Container_Tracing.jsp"
-// 		},
-// 		{
-// 			name: "Download Forms",
-// 			href: "/download.html"
-// 		},
-// 		{
-// 			name: "Railroad Circulars",
-// 			href: "/RCT.html"
-// 		}
-// 		,
-// 		{
-// 			name: "Useful links",
-// 			href: "/links.html"
-// 		}
-// 	]
-// })
+var jungleMenu = new Menu(menuContainer, configFilePath);
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -11,33 +11,72 @@
  * A layer that launches fireworks as directed.
  */
 class Menu {
-  //---------------------------------------------
-  // Constructor
-  //---------------------------------------------
-  /**
-   * Build a new Menu.
-   *
-   * @param {DOMElement} container
-   * @param {Object} options
-   */
-  constructor(container, $) {
+    //---------------------------------------------
+    // Constructor
+    //---------------------------------------------
+    /**
+     * Build a new Menu.
+     *
+     * @param {DOMElement} container
+     * @param {string} configFile
+     */
+    constructor(container, configFile) {
 
-    $(container).find(".item").on("click", function() {
+        var _this = this;
+        this.$container = $(container);
         
+        $.get(configFile, function(data) {
+            _this.menuConfigData = data;
+            $(document).ready(_this.onRenderReady.bind(_this));
+        });
+
+    }
+    
+    //---------------------------------------------
+    // Event Handlers
+    //---------------------------------------------
+        
+    /**
+     * bind all event handlers needed.
+     */
+    bindDOMElements() {
+        this.$container.find(".item").on("click", this.onMenuItemClick);
+        this.$container.find(".submenu").on("click", this.onSubMenuContainerClick);
+    }
+    
+    /**
+     * Handler when all render dependencies have loaded
+     */
+    onRenderReady() {
+
+        var renderTemplateWith = _.template($("#menuTemplate").html());
+        this.$container.html(renderTemplateWith(this.menuConfigData));
+        this.bindDOMElements();
+
+    }
+    /**
+     * Handle jquery event when a user clicks on a menu item.
+     */
+    onMenuItemClick() {
+
         var $this = $(this);
 
         if ($this.find(".submenu").length > 0) {
-          $this.toggleClass("open");
+            $this.toggleClass("open");
         }
-      
-    });
 
-  }
-  
+    }
+    /**
+     * Handle jquery event when a user clicks on a submenu container.
+     */
+    onSubMenuContainerClick(e) {
+         e.stopPropagation();
+    }
+
 }
 
 if (typeof window.Menu === "undefined") {
-  window.Menu = Menu;
+    window.Menu = Menu;
 }
 
 export default Menu;
